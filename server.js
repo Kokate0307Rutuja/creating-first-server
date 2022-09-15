d6e4c1e0ec1e18099c4e7d82780b52a0e06d429f
Kokate0307Rutuja <rutujakokate2000@gmail>
2022-09-16 04:57:04 +0530

    Complete the code for first node.js server

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const port = 8081; // local port no
+
+http
+    .createServer((req, res) => {
+        // callback function
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h1>Hello, this is from my server</h1>");
+        res.end();
+    })
+    .listen(port, () => {
+        // callback function
+        console.log(`Nodejs server started on port ${port}`);
+    });
+
+// http://localhost:8081
